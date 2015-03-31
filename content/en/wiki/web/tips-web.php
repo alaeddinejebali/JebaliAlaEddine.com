c710bea4b511f3a8220380748c23d9b7a0832692
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2015-03-31 17:51:39 +0100

    Update content/en/wiki/web/tips-web.php

---
+++ content/en/wiki/web/tips-web.php
@@ -0,0 +1,57 @@
+<div class="contentpanel" id="wiki">
+	<div class="row-fluid">
+		<div class="span12">
+			<section class="wiki-article">
+				<div class="wiki-article-title">
+					Title 1
+				</div>
+				<div class="wiki-article-body">
+					bbbbbb
+				</div>
+				<div class="wiki-article-title">
+					Title 2
+				</div>
+				<div class="wiki-article-body">
+					bbbbbb
+				</div>
+				<div class="wiki-article-title">
+					Title 3
+				</div>
+				<div class="wiki-article-body">
+					bbbbbb
+				</div>
+			</section>
+		</div>
+	</div>
+</div>
+
+
+
+<br/><br/><br/>
+The goal is to acceed <a href="#">http://localhost:8081/jebalialaeddine.com</a> using <a href="#">http://jebalialaeddine.dev:8081</a>
+
+
+Go to "C:\xampp\apache\conf\extra\httpd-vhosts.conf"
+Arround Line-19, add (or uncomment) this line
+NameVirtualHost *:8081
+At the bottom of the file add
+#Make "http://localhost:8081/jebalialaeddine.com" accessible via "http://jebalialaeddine.dev:8081"
+<VirtualHost *:8081>
+    ServerAdmin alaeddine.jebali@whitecapetech.com
+    DocumentRoot "C:/xampp/htdocs/jebalialaeddine.com"
+    ServerName jebalialaeddine.dev
+    ServerAlias jebalialaeddine.dev
+    <Directory "C:/xampp/htdocs/jebalialaeddine.com">
+        Order allow,deny
+        Allow from all
+    </Directory>
+</VirtualHost>
+
+
+Open Notepad++ as Administrator
+Open "C:\Windows\System32\drivers\etc\hosts"
+Go to the bottom of the file and add:
+127.0.0.1       localhost
+127.0.0.1       jebalialaeddine.dev
+
+Restart Apache
