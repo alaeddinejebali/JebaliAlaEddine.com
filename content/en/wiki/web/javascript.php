da50e2677fb67b984c5f30f5bd894b8f59792a88
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2015-05-03 22:38:37 +0100

    Javascript wiki

--- content/en/wiki/web/javascript.php
+++ content/en/wiki/web/javascript.php
@@ -264,6 +264,249 @@ switch(score){
 		break;
 }
 </code>
+</pre>
+					</p>
+					<h2>Looping</h2>
+					<h3>For</h3>
+					<p>
+<pre>
+<code class="javascript">
+var i;
+for(i=0 ; i<5 ; i++){
+	console.log("current iteration is: " + i);
+}
+</code>
+</pre>
+					</p>
+					<h3>For...in</h3>
+					<p>
+						- This loop works primarily with arrays and objects. It enables you to loop through each element in the array without having to know how many elements the array actually contains.
+<pre>
+<code class="javascript">
+var scores = new Array(10, 20, 30, 40, 50);
+for(score in scores){
+	console.log("current itertion is: " + score);
+	console.log("current value is: " + scores[score]);
+}
+</code>
+</pre>
+					</p>
+					<h3>While</h3>
+					<p>
+						- The while loop is more useful when you don’t know how many times you’ll need to loop.
+<pre>
+<code class="javascript">
+while(condition){
+	(...)
+}
+</code>
+</pre>
+					</p>
+					<h3>do while</h3>
+					<p>
+						- The code in the while loop executes at least once.
+<pre>
+<code class="javascript">
+do{
+	(...)
+}while(condition);
+</code>
+</pre>
+					</p>
+					<h3>The break statement</h3>
+					<p>
+						- the break statement can also be used as part of the for and while loops when you want to exit the loop prematurely.
+<pre>
+<code class="javascript">
+var scores = new Array(10, 20, -1, 30, 40, 50);
+for(score in scores){
+	if( scores[score]==-1 ){
+		alert("Score -1 reached, you will exit the loop");
+		break;
+	}
+	console.log("scores[" + score + "] = " + scores[score]);
+}
+//outputs: 10, 20 then exit
+</code>
+</pre>
+					</p>
+					<h3>The continue statement</h3>
+					<p>
+<pre>
+<code class="javascript">
+var scores = new Array(10, 20, -1, 30, 40, 50);
+for(score in scores){
+	if( scores[score]==-1 ){
+		alert("Score -1 reached, you will exit this iteration");
+		continue;
+	}
+	console.log("scores[" + score + "] = " + scores[score]);
+}
+//outputs: 10, 20
+//then display alert message
+//the outputs: 30, 40, 50
+</code>
+</pre>
+					</p>
+					<h3>Variables Scope</h3>
+					<ul>
+						<li>
+							- Any variables declared in a web page outside of a function will be available to all script on the page (whether that script is inside a function or otherwise). We term this a <i>global</i> or <i>page-level scope</i>.
+<pre>
+<code class="javascript">
+var score = 0;
+function updateScore(){
+	console.log("updateScore().score = " + score); //outputs: 0
+	score = 100;
+}
+updateScore();
+console.log("global.score = " + score); //outputs: 100
+</code>
+</pre>
+						</li>
+						<li>- Variables declared inside a function and function parameters are visible only inside that function.</li>
+						<li>
+							- Any variable declared without <i>var</i> has a global scope
+<pre>
+<code class="javascript">
+function updateScore(){
+	var variable_with_var = 50;
+	variable_without_var = 100;
+}
+updateScore();
+console.log("global.variable_with_var = " + variable_with_var);
+//outputs: variable_with_var is not defined
+console.log("global.variable_without_var = " + variable_without_var); //outputs: 100
+</code>
+</pre>
+						</li>
+						<li>- Variables lifetime: when the function fi nishes executing, the variables in that function die and their values are
+los</li>
+					</ul>
+				</div>
+			</section>
+			<section class="wiki-article">
+				<div class="wiki-article-title">
+					<h1 class="btnShowHideArticleBody" data-article-visibility="off">Debugging, and Error Handling<h1>
+				</div>
+				<div class="wiki-article-body invisible">
+					- Use parentheses at the end of the function name when you want to execute the function, and leave the parentheses off when passing the function to another function or property
+<pre>
+<code class="javascript">
+var user = 'anonymous';
+
+function sayHello(){
+	console.log("Hello " + user);
+}
+
+//getUser function accepts one argument that is a function.
+function getUser(greetUser){
+	user = 'Ala Eddine JEBALI';
+	greetUser();
+}
+
+//calls the getUser() function and passes a pointer of the sayHello() function
+getUser( sayHello );
+//outputs: Hello Ala Eddine JEBALI
+</code>
+</pre>
+					<h2>Error handling</h2>
+					<h3>The try ... catch Statements</h3>
+					<ul>
+						<li>- <i>exception.name</i> property contains the name of the error type</li>
+						<li>- <i>exception.message</i> property contains the error message the user would normally see</li>
+					</ul>
+					<p class="caution">
+						Before executing any code, the browser’s JavaScript engine goes through all the code and checks for syntax errors, or code that breaches JavaScript’s rules. If the engine fi nds a syntax error, the browser deals with it as usual.
+					</p>
+					<p>
+<pre>
+<code class="javascript">
+try{
+	console.log('This is code inside the try clause');
+	console.log('This code won'’'t work');
+}catch(exception){
+	console.log('exception: ' + exception.message);
+}
+//outputs: Unexpected identifier
+</code>
+</pre>
+					<h3>Trowing errors</h3>
+					<p class="caution">
+						Throwing errors can be very useful for indicating problems such as invalid user input. Rather than using lots of <i>if...else</i> statements, you can check the validity of user input, then use throw to stop code execution in its tracks and cause the error-catching code in the catch block of code to take over. In the catch clause, you can determine whether the error is based on user input, in which case you can notify the user what went wrong and how to correct it. Alternatively, if it’s an unexpected error, you can handle it more gracefully than with lots of JavaScript errors.
+					</p>
+					<p>
+<pre>
+<code class="javascript">
+function calculateFactorial(number){
+	var factorialResult = 1;
+	for (factorialResult; number > 0; number-- ){
+		factorialResult = factorialResult * number;
+	}
+	return factorialResult;
+}
+
+var nbr = prompt("Enter a number between 1 and 5");
+nbr = parseInt(nbr);
+try{
+	if( isNaN(nbr) ){
+		throw 'Please enter a number';
+	}else if(nbr<0){
+		throw 'Number should be positif';
+	}else if(nbr==0){
+		throw 'Number should not be zero';
+	}else if(nbr>5){
+		throw 'Number should not be greater than 5';
+	}
+	}else{
+		console.log('calculateFactorial(' + nbr + ') = ' + calculateFactorial(nbr));
+	}
+}catch(exception){
+	if( typeof(exception) == "string" ){
+		//exception thrown by your code, this instance you’ve thrown only primitive strings
+		console.log('A code exception encountered: ' + exception);
+	}else{
+		//it must be the browser that has generated this exception and that exception is an Exception object
+		console.log('A browser exception encountered: : ' + exception.message);
+	}
+}
+</code>
+</pre>
+					</p>
+					<h3>Finally Clauses</h3>
+					<p>
+						- It defines a block of code that will execute whether or not an exception was thrown.
+<pre>
+<code class="javascript">
+try{
+	(...)
+}catch(exception){
+	(...)
+}finally{
+	console.log('This code is always executed.');
+}
+</code>
+</pre>
+					</p>
+					<h3>Debugging</h3>
+					<ul>
+						<li>- <strong>Breakpoints</strong>: tell the debugger it should break, or pause code execution, at a certain point.</li>
+						<li>- <strong>Watches</strong>: allow you to specify variables that you want to inspect when your code pauses at a breakpoint.</li>
+						<li>- The <strong>call stack</strong>: is a record of what functions and methods have been executed to the breakpoint.</li>
+						<li>
+							- <strong>Stepping</strong> is the most common procedure in debugging. It allows you to execute one line of code at a time. There are three ways to step through code:
+							<ul>
+								<li>- <strong>Step Into</strong>: executes the next line of code. If that line is a function call, the debugger executes the function and halts at the first line of the function.</li>
+								<li>- <strong>Step Over</strong>: like Step Into, executes the next line of code. If that line is a function, Step Over executes the entire function and halts at the first line outside the function.</li>
+								<li>- <strong>Step Out</strong>: returns to the calling function when you are inside a called function. Step Out resumes the execution of code until the function returns. It then breaks at the return point of the function.</li>
+							</ul>
+						</li>
+						<li>- Using Firbug</li>
+						<li><img src="/content/images/javascript/01.png" /></li>
+						<li>
+							- The <strong>debugger</strong> Keyword: The debugger keyword stops the execution of JavaScript, and calls (if available) the debugging function. This has the same function as setting a breakpoint in the debugger.
+							<ul>
+								<li>
 
 
 
