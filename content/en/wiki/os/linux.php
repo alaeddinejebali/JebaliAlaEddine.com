55f4b737720354daca620306505248d474f21e70
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2015-05-04 17:30:22 +0100

    Add wiki.Linux

---
+++ content/en/wiki/os/linux.php
@@ -0,0 +1,70 @@
+<div class="contentpanel" id="wiki">
+	<div class="row-fluid">
+		<div class="span12">
+			<h1 class="wiki-title page-header"><span id="btnShowHideAllArticles" data-article-visibility="off" class="iconfa-plus-sign"></span> JavaScript</h1>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+
+			<section class="wiki-article">
+				<div class="wiki-article-title">
+					<h1 class="btnShowHideArticleBody" data-article-visibility="off">How to install JAVA?<h1>
+					<ul>
+						<li>update the package index</li>
+						<li class="noStyle">
+<pre>
+<code class="bash">
+$> sudo apt-get update
+</code>
+</pre>
+						</li>
+						<li>check if Java is not already installed:</li>
+						<li class="noStyle">
+<pre>
+<code class="bash">
+$> java -version
+</code>
+</pre>
+						</li>
+						<li>If it returns "The program java can be found in the following packages", Java hasn't been installed yet, so execute the following command:</li>
+						<li class="noStyle">
+<pre>
+<code class="bash">
+sudo apt-get install default-jdk
+</code>
+</pre>
+						</li>
+						<li>Check again</li>
+						<li class="noStyle">
+<pre>
+<code class="bash">
+$> sudo apt-get update
+</code>
+</pre>
+						</li>
+					</ul>
+				</div>
+				<div class="wiki-article-body invisible">
+					<ul>
+						<li>....</li>
+					</ul>
+				</div>
+			</section>
+
+
+
+			<section class="wiki-article">
+				<div class="wiki-article-title">
+					<h1 class="btnShowHideArticleBody" data-article-visibility="off">To be prepared<h1>
+				</div>
+				<div class="wiki-article-body invisible">
+					<ul>
+						<li>....</li>
+					</ul>
+				</div>
+			</section>
+
+		</div>
+	</div>
+</div>
