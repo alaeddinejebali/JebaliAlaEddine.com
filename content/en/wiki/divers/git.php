bf4823c9443e9ce90e50f37e28a2a28e7126dda3
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2015-07-14 01:30:18 +0200

    GIT - How to check for remote updates without pull?

--- content/en/wiki/divers/git.php
+++ content/en/wiki/divers/git.php
@@ -6,6 +6,7 @@
     </div>
     <div class="row-fluid">
         <div class="span12">
+
             <section class="wiki-article">
                 <div class="wiki-article-title">
                     <h1 class="btnShowHideArticleBody" data-article-visibility="off">How to color the Git console in Ubuntu??<h1>
@@ -14,6 +15,25 @@
                         <li class="noStyle">
                             Open "home/userName/.gitconfig" file (Ctrl+H to display hidden files) and edit it like this:
                             <?php include "code/git_code_01.txt"; ?>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+
+            <section class="wiki-article">
+                <div class="wiki-article-title">
+                    <h1 class="btnShowHideArticleBody" data-article-visibility="off">How to check for remote updates without Pull?<h1>
+                </div>
+                <div class="wiki-article-body invisible">
+                    <ul>
+                        <li>Update the remote branch in your repository to point to the latest version
+                            $> git fetch origin
+                        </li>
+                        <li>Show difference against remote
+                            $> git diff HEAD origin/master
+                        </li>
+                        <li>Accept (if you're okay) the remote changes:
+                            $> git merge origin/master
                         </li>
                     </ul>
                 </div>
