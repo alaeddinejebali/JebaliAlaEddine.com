16e65e3f7c0aef9bf1615f19316bdb2dd0fc6e97
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2015-08-30 14:03:26 +0200

    update sidebar

--- content/en/sidebar.php
+++ content/en/sidebar.php
@@ -1,21 +1,90 @@
 <!-- START OF LEFT PANEL -->
 <div class="leftpanel">
-	
-    <div class="logopanel">
-    	<a href="/"><img src="/assets/images/logo.png" alt="Ala Eddine JEBALI" /></a>
-    </div><!--logopanel-->
-    
-    <div class="searchpanel">
-    	<form action="index.html" method="post">
-           <input type="text" name="keyword" placeholder="Search and hit enter..." />
-       </form>
-   </div><!--searchpanel-->
 
-   <div class="menupanel">
-       <ul>
+    <div class="logopanel">
+        <a href="/"><img src="/assets/images/logo.png" alt="Ala Eddine JEBALI"/></a>
+    </div>
+    <!--logopanel-->
+
+    <div class="searchpanel">
+        <form action="index.html" method="post">
+            <input type="text" name="keyword" placeholder="Search and hit enter..."/>
+        </form>
+    </div>
+    <!--searchpanel-->
+
+    <div class="menupanel">
+        <ul>
             <li class="<?= $currentActivePage['home'] ?>">
                 <a href="/"><span class="iconfa-home"></span> Home</a>
             </li>
+            <?php if (!empty($_SESSION['login.loggedUser'])) { ?>
+                <li class="<?= $currentActivePage['wiki'] ?>">
+                    <a href=""><span class="iconfa-star"></span> Wiki</a><i
+                        class="iconfa-angle-right sub-indicator"></i>
+                    <ul>
+                        <li>
+                            <a href="company.html">Java/J2EE</a><i class="iconfa-angle-right sub-indicator"></i>
+                            <ul>
+                                <li><a href="company.html">Java</a></li>
+                                <li><a href="ourteam.html">Hibernate</a></li>
+                                <li><a href="careers.html">Spring</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="company.html">Mobile</a><i class="iconfa-angle-right sub-indicator"></i>
+                            <ul>
+                                <li><a href="company.html">Android</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">PHP</a><i class="iconfa-angle-right sub-indicator"></i>
+                            <ul>
+                                <li><a href="ourteam.html">CakePHP</a></li>
+                                <li><a href="/wiki/php/php5">PHP5</a></li>
+                                <li><a href="/wiki/php/symfony">Symfony</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="company.html">Web</a><i class="iconfa-angle-right sub-indicator"></i>
+                            <ul>
+                                <li>
+                                    <a href="#">JavaScript</a><i
+                                        class="iconfa-angle-right sub-indicator"></i>
+                                    <ul>
+                                        <li>
+                                            <a href="/wiki/web/javascript">JavaScript</a>
+                                        <li>
+                                            <a href="/wiki/web/angularjs">AngularJS</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="ourteam.html">Design</a><i class="iconfa-angle-right sub-indicator"></i>
+                                    <ul>
+                                        <li>
+                                            <a href="">Bootstrap</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="/wiki/web/tips-web">Trucs web</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="company.html">Others</a><i class="iconfa-angle-right sub-indicator"></i>
+                            <ul>
+                                <li><a href="/wiki/divers/git">GIT</a></li>
+                                <li><a href="/wiki/divers/ide">IDE</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="/logout"><span class="iconfa-lock"></span> Logout</a>
+                </li>
+            <?php } ?>
             <li class="<?= $currentActivePage['aboutMe'] ?>">
                 <a href="/about-me"><span class="iconfa-user-md"></span> About me</a>
             </li>
@@ -28,74 +97,12 @@
             <li class="<?= $currentActivePage['blog'] ?>">
                 <a href="/blog"><span class="iconfa-file"></span> Blog</a>
             </li>
-            <?php if( !empty($_SESSION['login.loggedUser']) ){ ?>
-            <li class="<?= $currentActivePage['wiki'] ?>">
-                <a href=""><span class="iconfa-star"></span> Wiki</a><i class="iconfa-angle-right sub-indicator"></i>
-                <ul>
-                    <li>
-                        <a href="company.html">Java/J2EE</a><i class="iconfa-angle-right sub-indicator"></i>
-                        <ul>
-                            <li><a href="company.html">Java</a></li>
-                            <li><a href="ourteam.html">Hibernate</a></li>
-                            <li><a href="careers.html">Spring</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="company.html">Mobile</a><i class="iconfa-angle-right sub-indicator"></i>
-                        <ul>
-                            <li><a href="company.html">Android</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#">PHP</a><i class="iconfa-angle-right sub-indicator"></i>
-                        <ul>
-                            <li><a href="ourteam.html">CakePHP</a></li>
-                            <li><a href="/wiki/php/php5">PHP5</a></li>
-                            <li><a href="careers.html">Symfony2</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="company.html">Web</a><i class="iconfa-angle-right sub-indicator"></i>
-                        <ul>
-                            <li>
-                                <a href="/wiki/web/javascript">JavaScript</a><i class="iconfa-angle-right sub-indicator"></i>
-                                <ul>
-                                    <li>
-                                        <a href="/wiki/web/javascript">JavaScript</a>
-                                    <li>
-                                        <a href="">AngularJS</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="ourteam.html">Design</a><i class="iconfa-angle-right sub-indicator"></i>
-                                <ul>
-                                    <li>
-                                        <a href="">Bootstrap</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="/wiki/web/tips-web">Trucs web</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="company.html">Others</a><i class="iconfa-angle-right sub-indicator"></i>
-                        <ul>
-                            <li><a href="/wiki/divers/git">GIT</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-                <li>
-                    <a href="/logout"><span class="iconfa-lock"></span> Logout</a>
+            <?php if (empty($_SESSION['login.loggedUser'])) { ?>
+                <li class="<?= $currentActivePage['login'] ?>">
+                    <a href="/login"><span class="iconfa-unlock"></span> Login</a>
                 </li>
-            <?php }else{ ?>
-            <li class="<?= $currentActivePage['login'] ?>">
-                <a href="/login"><span class="iconfa-unlock"></span> Login</a>
-            </li>
             <?php } ?>
         </ul>
-    </div><!--menupanel-->
+    </div>
+    <!--menupanel-->
 </div><!--leftpanel-->
