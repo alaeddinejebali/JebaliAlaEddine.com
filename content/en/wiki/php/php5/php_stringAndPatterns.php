7688a308182c0bf35669334af03d7add0db99683
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2016-03-13 21:37:43 +0100

    Add PHP wiki

--- content/en/wiki/php/php5/php_stringAndPatterns.php
+++ content/en/wiki/php/php5/php_stringAndPatterns.php
@@ -151,7 +151,173 @@
         		<h2>Formatting strings</h2>
         		<ul>
         			<li>
-        			
+        				Formatting Numbers
+        				<ul>
+        					<li>
+        						number_format($number, $optionalNbrDecimal=0, $optionalSeparatorDecimal=".", $optionalThousendSeparator=",")
+        						<ul>
+        							<li>
+        								This function accepts either one, two, or four parameters (not three).
+        							</li>
+        							<li>
+        								number_format($number): The number will be rounded to the nearest integer, and a comma will be used to separate thousands.
+        								<?php include "code/php_code_68.txt"; ?>
+        							</li>
+        							<li>
+        								This function is not "locale-aware": Even if you have a French or German locale set ( with setlocale (LC_MONETARY, 'fr_FR'); ), it will still use periods for decimals and commas for thousands, unless you specify otherwise.
+        							</li>
+        						</ul>
+        					</li>
+        				</ul>
+        			</li>
+        		</ul>
+        	</li>
+        	<li>
+        		<h2>Formatting Currency Values</h2>
+        		<ul>
+        			<li>
+        				This function is "locale aware" and will display the correct currency symbol (<u>n</u>ational or <u>i</u>nternational notation) depending on how your locale is set.
+        				<?php include "code/php_code_69.txt"; ?>
+        			</li>
+        			<li class="caution">
+        				call to setlocale() affects the entire process inside which it is executed, rather than the individual script. Thus, you should be careful to always reset the locale whenever you need to perform a formatting operation.
+        			</li>
+        			<li>
+        				The default rounding rules may change from locale to locale.
+        				<?php include "code/php_code_70.txt"; ?>
+        			</li>
+        		</ul>
+        	</li>
+        	<li>
+        		<h2>Generic Formatting</h2>
+        		<ul>
+        			<li>
+        				If you are not handling numbers or currency values, you can use the printf() family of functions to perform arbitrary formatting of a value.
+        				<?php include "code/php_code_71.txt"; ?>
+        			</li>
+        		</ul>
+        	</li>
+        	<li>
+        		<h2>Parsing Formatted Input</h2>
+        		<ul>
+        			<li>
+        				works in a similar way to printf() , except that, instead of formatting output, it allows you to parse formatted input
+        				<?php include "code/php_code_72.txt"; ?>
+        			</li>
+        			<li>
+        				The data must match the format passed to sscanf() exactly—or the function will fail to retrieve all the values.
+        			</li>
+        		</ul>
+        	</li>
+        	<li>
+        		<h2>Perl-compatible Regular Expressions (PCRE)</h2>
+        		<ul>
+        			<li>
+        				Offer a very powerful string-matching and replacement mechanism.
+        			</li>
+        			<li>
+        				The real power of regular expressions comes into play when you don't know the exact string that you want to match.
+        			</li>
+        			<li>
+        				Delimiter
+        				<ul>
+        					<li>
+        						A regular expression is always delimited by a starting and ending character.
+        					</li>
+        					<li>
+        						it's usually a good idea to pick a delimiter that isn't likely to appear inside the expression.
+        					</li>
+        					<li>
+        						By convention, the forward slash is used for this purpose.
+        					</li>
+        				</ul>
+        			</li>
+        			<li>
+        				Metacharacters
+        				<ul>
+        					<li class="noStyle">"." : Match any character</li>
+        					<li class="noStyle">"^" : Match the start of the string</li>
+        					<li class="noStyle">"$" : Match the end of the string</li>
+        					<li class="noStyle">"\s": Match any whitespace character</li>
+        					<li class="noStyle">"\d": Match any digit</li>
+        					<li class="noStyle">"\w": Match any word character</li>
+        					<li class="noStyle">"[]": Ranges of valid characters</li>
+        				</ul>
+        			</li>
+        			<li>
+        				Quantifiers
+        				<ul>
+        					<li class="noStyle">"*": zero or more times</li>
+        					<li class="noStyle">"+": one or more times</li>
+        					<li class="noStyle">"?": zero or one times</li>
+        					<li class="noStyle">{n,m}: at least n times, and no more than m .</li>
+        				</ul>
+        			</li>
+        			<li>
+        				Matching and Extracting Strings
+        				<ul>
+        					<li>
+        						preg_match($pattern, $string)
+        						<ul>
+        							<li>
+        								returns 1 if the match is successful, 0 otherwise.
+        							</li>
+        						</ul>
+        					</li>
+        					<li>
+        						preg_match($pattern, $string, $matches)
+        						<ul>
+        							<li>
+        								return all the captured subpatterns.
+        								<?php include 'code/php_code_73.txt'; ?>
+        							</li>
+        							<li class="caution">
+        								The first element of the array contains the entire matched string.
+        							</li>
+        						</ul>
+        					</li>
+        				</ul>
+        			</li>
+        			<li>
+        				Performing Multiple Matches
+        				<ul>
+        					<li>
+        						preg_match_all($pattern, $string, $matches)
+        						<ul>
+        							<li>
+        								All the whole-pattern matches are stored in the first sub-array of the result.
+									</li>
+									<li>
+										The first captured subpattern of every match is stored in the corresponding slot of the second sub-array.
+        								<?php include 'code/php_code_74.txt'; ?>
+        							</li>
+        						</ul>
+        					</li>
+        				</ul>
+        			</li>
+        			<li>
+        				Using PCRE to Replace Strings
+        				<ul>
+		        			<li>
+		        				str_replace() is quite flexible, it still only works on “whole” strings, that is, where you know the exact text to search for.
+		        			</li>
+        					<li>
+        						preg_replace($regex, $replacement, $string)
+        						<ul>
+		        					<li>
+		        						Replace text that matches a pattern we specify.
+		        					</li>
+		        					<li>
+		        						It is possible to reuse captured subpatterns directly in the substitution string by prefixing their index with a dollar sign.
+		        						<?php include 'code/php_code_75.txt'; ?>
+		        					</li>
+		        					<li>
+		        						We can pass arrays of search and replacement arguments and we can also pass in an array of subjects.
+		        						<?php include 'code/php_code_76.txt'; ?>
+		        					</li>
+	        					</ul>
+        					</li>
+        				</ul>
         			</li>
         		</ul>
         	</li>
--- content/en/wiki/php/php5/php_web.php
+++ content/en/wiki/php/php5/php_web.php
@@ -4,7 +4,74 @@
     </div>
     <div class="wiki-article-body invisible">
         <ul>
-            <li>Hello</li>
+            <li>
+        		<h2>Form and URLs</h2>
+        		<ul>
+        			<li>
+        				You can create arrays by using array notation:
+        				<?php include 'code/php_code_77.txt'; ?>
+        			</li>
+        			<li>
+        				urlencode($data)
+        				<ul>
+        					<li class="noStyle">
+        						<?php include 'code/php_code_78.txt'; ?>
+        					</li>
+        				</ul>
+        			</li>
+        			<li>
+        				Verify submit button is clicked
+        				<ul>
+        					<li class="noStyle">
+        						<?php include 'code/php_code_79.txt'; ?>
+        					</li>
+        				</ul>
+        			</li>
+        			<li>
+        				$_REQUEST: is used when you don't know how data is sent (using $GET or $POST)
+        			</li>
+        			<li>
+        				Managing File Uploads
+        				<ul>
+        					<li class="noStyle">
+        						<?php include 'code/php_code_80.txt'; ?>
+        					</li>
+        					<li>
+        						As MAX_FILE_SIZE is defined in client side, you can limit the amount of data uploaded by a POST operation by modifying a number of configuration directives, such as <b>post_max_size</b>, <b>max_input_time</b> and <b>upload_max_filesize</b> in php.ini.
+        					</li>
+        					<li>
+        						Once a file is uploaded to the server, PHP stores it in a temporary location and makes it available to the script that was called by the POST transaction.
+        					</li>
+        					<li>
+        						The temporary copy is automatically destroyed when the script ends.
+        					</li>
+        					<li>
+        						Inside your script, uploaded files will appear in the $_FILES superglobal array.
+        					</li>
+        					<li class="noStyle">
+        						<?php include 'code/php_code_81.txt'; ?>
+        					</li>
+        					<li>
+        						is_uploaded_file(file)
+        						<ul>
+        							<li>
+        								Checks whether the specified file is uploaded via HTTP POST.
+        								<?php include 'code/php_code_82.txt'; ?>
+        							</li>
+        						</ul>
+        					</li>
+        				</ul>
+        			</li>
+        		</ul>
+        	</li>
+        	<li>
+        		<h2>Sessions</h2>
+        		<ul>
+        			<li>
+        				HTTP is a stateless protocol; this means that the webserver does not know whether two requests comes from the same user.
+        			</li>
+        		</ul>
+        	</li>
         </ul>
     </div>
 </section>
