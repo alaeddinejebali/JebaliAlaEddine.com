ed7823cbd18d0289111843073d55185048a77258
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2016-05-04 15:38:45 +0200

    Update sidebar

--- content/en/wiki/web/angularjs1/index.php
+++ content/en/wiki/web/angularjs1/index.php
@@ -1,7 +1,7 @@
 <div class="contentpanel" id="wiki">
     <div class="row-fluid">
         <div class="span12">
-            <h1 class="wiki-title page-header"><span id="btnShowHideAllArticles" data-article-visibility="off" class="iconfa-plus-sign"></span> PHP5</h1>
+            <h1 class="wiki-title page-header"><span id="btnShowHideAllArticles" data-article-visibility="off" class="iconfa-plus-sign"></span> Angular 1</h1>
         </div>
     </div>
     <div class="row-fluid">
