037e045e2886f0cabcd1c7b62789fba82c9a9602
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2016-05-24 16:45:51 +0200

    Update java wiki

---
+++ content/en/wiki/java/javacore/java_introduction.php
@@ -0,0 +1,15 @@
+<section class="wiki-article">
+	<div class="wiki-article-title">
+		<h1 class="btnShowHideArticleBody" data-article-visibility="off"> Introduction<h1>
+	</div>
+	<div class="wiki-article-body invisible">
+		<ul>
+			<li>
+				<h2>Install Java-8</h2>
+				<ul>
+					<li class="noStyle"><?php include 'code/java_code_09.txt'; ?></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</section>
