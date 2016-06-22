257a0ae4c5f39f6aef0e7058d6a81ea875c36cb9
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2016-06-22 16:58:06 +0200

    Add Java wiki

---
+++ content/en/wiki/java/javacore/java_looping.php
@@ -0,0 +1,32 @@
+<section class="wiki-article">
+	<div class="wiki-article-title">
+		<h1 class="btnShowHideArticleBody" data-article-visibility="off"> Operators and statements<h1>
+	</div>
+	<div class="wiki-article-body invisible">
+		<ul>
+			<li>
+				<h2>Install Java-8</h2>
+				<ul>
+					<li>
+						An <i>operator</i> is a special symbol that can be applied to operands (a set of variables, values, or literals) and that returns a result.
+					</li>
+					<li>
+						Java operators are not necessarily evaluated from left-to-right order.
+						<ul>
+							<li>
+								Example:
+								<?php include "code/java_15.txt"; ?>
+								<ol>
+									<li>Decrement y to 3</li>
+									<li>Multiply the resulting value by 2</li>
+									<li>Add 3</li>
+									<li>x has value 9.0 and y has value 3</li>
+								</ol>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</section>
