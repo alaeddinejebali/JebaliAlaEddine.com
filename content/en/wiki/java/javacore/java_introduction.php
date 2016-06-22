ecd275ede6d47c9c2341de3f55b2731f7c23a177
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2016-06-22 16:57:58 +0200

    Add Java wiki

--- content/en/wiki/java/javacore/java_introduction.php
+++ content/en/wiki/java/javacore/java_introduction.php
@@ -10,6 +10,66 @@
 					<li class="noStyle"><?php include 'code/java_code_09.txt'; ?></li>
 				</ul>
 			</li>
+			<li>
+				<h2>aaa</h2>
+				<ul>
+					<li>
+						Java is an interpreted language because it gets compiled to bytecode.
+					</li>
+					<li>
+						Java code gets compiled once rather than needing to be recompiled for different operating systems. This is known as "write once, run everywhere.".
+					</li>
+				</ul>
+			</li>
+			<li>
+				<h2>Benefits of Java</h2>
+				<ul>
+					<li>
+						Object Oriented
+					</li>
+					<li>
+						Encapsulation
+						<ul>
+							<li>
+								Java supports access modifi ers to protect data from unintended access and modifi cation.
+							</li>
+						</ul>
+					</li>
+					<li>
+						Platform Independent
+						<ul>
+							<li>
+								Thanks to "write once, run everywhere."
+							</li>
+						</ul>
+					</li>
+					<li>
+						Robust
+						<ul>
+							<li>
+								One of the major advantages of Java over C++ is that it prevents memory leaks.
+							</li>
+							<li>
+								Java manages memory on its own and does garbage collection automatically.
+							</li>
+						</ul>
+					</li>
+					<li>
+						Simple
+					</li>
+					<li>
+						Secure
+						<ul>
+							<li>
+								Java code runs inside the JVM.
+							</li>
+							<li>
+								This creates a sandbox that makes it hard for Java code to do evil things to the computer it is running on.
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</li>
 		</ul>
 	</div>
 </section>
