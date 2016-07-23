a264276df53a21898613499f6b891b889704377f
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2016-07-23 20:48:35 +0200

    Update Design patterns wiki

---
+++ content/en/wiki/model.php
@@ -0,0 +1,10 @@
+<section class="wiki-article">
+	<div class="wiki-article-title">
+    	<h1 class="btnShowHideArticleBody" data-article-visibility="off">Title<h1>
+    </div>
+    <div class="wiki-article-body invisible">
+    	<ul>
+			<li></li>
+		</ul>
+	</div>
+</section>
