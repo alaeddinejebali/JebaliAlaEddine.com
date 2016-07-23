af773a4a2beb8d9d9070c327d59fe5afe6ca81a5
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2016-07-23 20:48:03 +0200

    Update NodeJS wiki

--- content/en/wiki/frontend/nodejs/installation.php
+++ content/en/wiki/frontend/nodejs/installation.php
@@ -3,49 +3,53 @@
         <h1 class="btnShowHideArticleBody" data-article-visibility="off">Installation on Linux<h1>
     </div>
     <div class="wiki-article-body invisible">
-		<h2>Verify if node is installed</h2>
-        	<ul>
-            	<li>
-                	Check Node version
-                	<?php include "code/nodejs_code_02.txt"; ?>
-                </li>
-			</ul>
-    	<h2>Method 1: Automatic installation</h2>
     	<ul>
     		<li>
-    			Run:
-				<?php include "code/nodejs_code_18.txt"; ?>
+    			<h2>Verify if NodeJS is installed</h2>
+	        	<ul>
+	            	<li>
+	                	Check Node version
+	                	<?php include "code/nodejs_code_02.txt"; ?>
+	                </li>
+				</ul>
+    		</li>
+    		<li>
+		    	<h2>Method 1: Automatic installation</h2>
+		    	<ul>
+		        	<li class="noStyle">
+		        		<?php include "code/nodejs_code_18.txt"; ?>
+		        	</li>
+		    	</ul>    		
+    		</li>
+    		<li>
+		    	<h2>Method 2: Manual installation</h2>
+		    	<ul>
+					<li>
+		            	Go to <a href="https://nodejs.org/en/" target="_blank">https://nodejs.org/en/</a> and download the archive file (node-v4.2.2-linux-x64.tar.gz)
+					</li>
+					<li>
+						Then:	
+						<ul>
+		                	<li class="noStyle">
+		                		<?php include "code/nodejs_code_01.txt"; ?>
+		                	</li>
+		                </ul>
+					</li>
+		    	</ul>    		
+    		</li>
+    		<li>
+				<h2>Run a Hello world example</h2>
+				<ul>
+					<li class="noStyle">
+						<?php include "code/nodejs_code_03.txt"; ?>			
+					</li>
+					<li class="caution">
+						If you encounter this problem "/usr/bin/env: node: No such file or directory",
+						You solve it using this command:
+						<?php include "code/nodejs_code_17.txt"; ?>
+					</li>
+				</ul>    		
     		</li>
     	</ul>
-    	<h2>Method 2: Manual installation</h2>
-    	<ul>
-			<li>
-            	Go to <a href="https://nodejs.org/en/" target="_blank">https://nodejs.org/en/</a> and download the archive file (node-v4.2.2-linux-x64.tar.gz)
-				<?php include "code/nodejs_code_01.txt"; ?>
-			</li>
-    	</ul>
-		<h2>Run a Hello world example</h2>
-		<ul>
-			<li>
-				<?php include "code/nodejs_code_03.txt"; ?>			
-			</li>
-			<li class="caution">
-				If you encounter this problem "/usr/bin/env: node: No such file or directory",
-				You solve it using this command:
-				<?php include "code/nodejs_code_17.txt"; ?>
-			</li>
-		</ul>
-		
-		
-		////////////////////////////////////////////
-		How to read passed parameters
-		We need to say hello to all users passed in parameters
-		<?php include "code/nodejs_code_19.txt"; ?>
-		Code:
-		<?php include "code/nodejs_code_20.txt"; ?>
-		<li class="caution">The first element will be 'node', the second element will be the name of the JavaScript file. The next elements will be any additional command line arguments.</li>
-		////////////////////////////////////////////
-		
-		
     </div>
 </section>
---
+++ content/en/wiki/frontend/nodejs/tips.php
@@ -0,0 +1,39 @@
+<section class="wiki-article">
+	<div class="wiki-article-title">
+    	<h1 class="btnShowHideArticleBody" data-article-visibility="off">Tips and Trips<h1>
+    </div>
+    <div class="wiki-article-body invisible">
+    	<ul>
+			<li>
+				<h2>How to read passed parameters?</h2>
+				<ul>
+					<li>
+						We need to say "Hello" to all users passed in parameters
+						<ul>
+							<li class="noStyle">
+								<?php include "code/nodejs_code_20.txt"; ?>
+							</li>
+							<li>
+								Using <i>process.argv</i>, the first element will be 'node'
+							</li>
+							<li>
+								The second element will be the name of the JavaScript file.
+							</li>
+							<li>	
+								The next elements will be any additional command line arguments.
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</li>
+			<li>
+				<h2>Read file</h2>
+				<ul>
+					<li class="noStyle">
+						<?php include "code/nodejs_code_22.txt"; ?>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</section>
