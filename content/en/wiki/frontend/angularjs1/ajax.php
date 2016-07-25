b302f584f9b27f0a93d110db0c39ec1419fa3111
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2016-07-25 16:23:12 +0200

    Update AngularJS wiki

---
+++ content/en/wiki/frontend/angularjs1/ajax.php
@@ -0,0 +1,31 @@
+<section class="wiki-article">
+    <div class="wiki-article-title">
+        <h1 class="btnShowHideArticleBody" data-article-visibility="off">AJAX<h1>
+    </div>
+    <div class="wiki-article-body invisible">
+        <ul>
+			<li>
+				<h2>using $http</h2>
+				<ul>
+					<li>
+						$http is an XMLHttpRequest object for requesting external data.
+					</li>
+					<li>
+						Example: suppose that the page http://www.website.com/page.php returns
+						<?php include 'code/angular_01.txt'; ?>
+						Html file:
+						<?php include 'code/angular_02.txt'; ?>
+					</li>
+				</ul>
+			</li>
+			<li>
+				<h2><b>$index</b>, <b>$even</b> and <b>$odd</b> elements</h2>
+				<ul>
+					<li class="noStyle">
+						<?php include 'code/angular_02.txt'; ?>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</section>
