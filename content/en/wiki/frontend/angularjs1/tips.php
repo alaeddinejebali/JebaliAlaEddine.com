1d054bed1934d08e691ba3b4b968a31cc31fdd26
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2016-07-25 16:23:30 +0200

    Update AngularJS wiki

---
+++ content/en/wiki/frontend/angularjs1/tips.php
@@ -0,0 +1,40 @@
+<section class="wiki-article">
+    <div class="wiki-article-title">
+        <h1 class="btnShowHideArticleBody" data-article-visibility="off">Tips and Trics<h1>
+    </div>
+    <div class="wiki-article-body invisible">
+        <ul>
+        	<li>
+        		<h2>Angular-1 vs Angular-2</h2>
+        		<ul>
+        			<li>
+        				aaa
+        			</li>
+        		</ul>
+        	</li>
+			<li>
+				<h2>Including a portion of HTML in HTML</h2>
+				<ul>
+					<li>Not (yet) supported by HTML</li>
+					<li>
+						With AngularJS, you can include HTML content using the <b>ng-include</b> directive:
+						<?php include 'code/angular_12.txt'; ?>
+					</li>
+				</ul>
+			</li>
+			<li>
+				<h2>Using Bootstrap</h2>
+				<ul>
+					<li>
+						Layout
+						<?php include 'code/angular_11.txt'; ?>
+					</li>
+					<li>
+						Script
+						<?php include 'code/angular_12.txt'; ?>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</section>
