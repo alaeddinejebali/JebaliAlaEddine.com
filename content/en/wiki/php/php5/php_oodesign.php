794b6181f1cb23e9c5a93312fe603201b71c9af7
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2016-07-31 23:11:43 +0200

    Update IDE wiki

--- content/en/wiki/php/php5/php_oodesign.php
+++ content/en/wiki/php/php5/php_oodesign.php
@@ -5,115 +5,13 @@
     <div class="wiki-article-body invisible">
         <ul>
             <li>
-                <h2>Design Pattern Theory</h2>
-                <ul>
-                	<li>
-                		Design Patterns are generally recognized as an excellent set of tried-and-true solutions to common problems that developers face every day.
-                	</li>
-                	<li>
-                		They simply provide guidelines that developers can translate into code.
-                	</li>
-                </ul>
-            <li>
-            <li>
-            	<h2>Design patterns</h2>
+            	<h2>Most know design patterns</h2>
             	<ul>
-		            <li class="noStyle">
-		            	<h3>The Singleton Pattern</h3>
-		            	<ul>
-		            		<li>
-		            			Its goal is to provide access to a single resource that is never duplicated, but that is made available to any portion of an application that requests it without the need to keep track of its existence
-		            		</li>
-		            		<li>
-		            			The most typical example of this pattern is a database connection, which normally only needs to be created once at the beginning of a script and then used throughout its code.
-		            		</li>
-		            		<li>
-		            			Example:
-		            			<ul>
-		            				<li class="noStyle">
-		            					<?php include 'code/php_code_124.txt'; ?>
-		            				</li>
-		            			</ul>
-		            			getInstance() will never attempt to create a new instance of DB , and instead always return the initialized $_connection , thus ensuring that a database connection is not created more than once.
-		            		</li>
-		            	</ul>
-		            </li>
-		            <li class="noStyle">
-		            	<h3>The Factory Pattern</h3>
-		            	<ul>
-		            		<li>
-		            			It is used in scenarios where you have a generic class (<u>the factory</u>) that provides <u>the facilities</u> for creating instances of one or more separate "specialized" classes that handle the same task in different ways.
-		            		</li>
-		            		<li>
-		            			A good situation in which the Factory pattern provides an excellent solution is the management of multiple storage mechanisms for a given task (ini file, database or xml file).
-								<ul>
-									<li class="noStyle">
-										<?php include 'code/php_code_125.txt'; ?>
-									</li>
-								</ul>
-		            		</li>
-		            	</ul>
-		            </li>
-		            <li class="noStyle">
-		            	<h3>The Registry Pattern</h3>
-		            	<ul>
-		            		<li>
-		            			When you want to find an object you usually start with another object that has an association to it ( <i>$vClient.findById(22)</i> ).
-		            		</li>
-		            		<li>
-		            			However, in some cases you won't have an appropriate object to start with (<i>$vClient</i>). You may know the customer's ID number (<i>22</i>) but not have a reference.
-		            		</li>
-		            		<li>
-		            			In this case you need some kind of lookup method: <u>a finder</u>.
-		            		</li>
-		            		<li>
-		            			A Registry is essentially a global object, or at least it looks like one - even if it isn't as global as it may appear.
-		            			<ul>
-		            				<li class="noStyle">
-		            					<?php include 'code/php_code_123.txt'; ?>
-		            				</li>
-		            			</ul>
-		            		</li>
-		            	</ul>
-		            </li>
-		            <li class="noStyle">
-		            	<h3>The Model-View-Controller Pattern</h3>
-		            	<ul>
-		            		<li>
-		            			Its goal is that of providing a methodology for separating the business logic (model) from the display logic (view) and the decisional controls (controller).
-		            		</li>
-		            		<li>
-		            			Process:
-		            			<ol>
-		            				<li>
-		            					User initiates an action by callingthe Controller.
-		            				</li>
-		            				<li>
-		            					Controller interfaces with the Model (causing it to perform some sort of action and, therefore, changing its state).
-		            				</li>
-		            				<li>
-		            					View is called, thus causing the user interface to be refreshed to reflect the changes in the Model.
-		            				</li>
-		            			</ol>
-		            		</li>
-		            	</ul>
-		            </li>
-		            <li class="noStyle">
-		            	<h3>The ActiveRecord Pattern</h3>
-		            	<ul>
-		            		<li>
-		            			This pattern is commonly used by object persistence tools and in object-relational mapping (ORM).
-		            		</li>
-		            		<li>
-		            			The active record pattern is an approach to accessing data in a database.
-		            		</li>
-		            		<li>
-		            			 A database table or view is wrapped into a class. Thus, an object instance is tied to a single row in the table. After creation of an object, a new row is added to the table upon save. Any object loaded gets its information from the database. When an object is updated the corresponding row in the table is also updated. The wrapper class implements accessor methods or properties for each column in the table or view.
-		            		</li>
-		            	</ul>
-		            </li>
-		    	</ul>
-			</li>
+            		<li>
+            			<a href="/wiki/designpatterns">See this page</a>.
+            		</li>
+            	</ul>
+            </li>
 			<li>
 				<h2>SPL: The Standard PHP Library</h2>
 				<ul>
