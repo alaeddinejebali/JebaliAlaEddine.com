2c8609272c309d07f7d4a3ceeca4c994795ae2eb
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2016-09-13 07:07:20 +0200

    update wiki

--- content/en/sidebar.php
+++ content/en/sidebar.php
@@ -29,19 +29,6 @@
 					<li><a href="/java/spring">Spring</a></li>
 				</ul></li>
 			<li class="<?= $currentActivePage['oop'] ?>"><a href="/oop"><span class="iconfa-th"></span> OOP</a></li>
-			<li class="<?= $currentActivePage['php'] ?>"><a href="#"><span class="iconfa-refresh"></span> PHP</a><i
-				class="iconfa-angle-right sub-indicator"></i>
-				<ul>
-					<li><a href="/php/php5">PHP5</a></li>
-					<li><a href="/php/symfony">Symfony</a></li>
-				</ul></li>
-			<li class="<?= $currentActivePage['web'] ?>"><a href="company.html"><span class="iconfa-picture"></span> Web</a><i
-				class="iconfa-angle-right sub-indicator"></i>
-				<ul>
-					<li><a href="/web/firefox-addons">Firefox addons</a></li>
-					<li><a href="/web/tips-web">Trucs web</a></li>
-				</ul></li>
-			<li class="<?= $currentActivePage['xml'] ?>"><a href="/xml"><span class="iconfa-random"></span> XML</a></li>
 			<li class="<?= $currentActivePage['others'] ?>"><a
 				href="company.html"><span class="iconfa-wrench"></span> Others</a><i
 				class="iconfa-angle-right sub-indicator"></i>
@@ -53,6 +40,26 @@
 					<li><a href="/divers/linux">Linux</a></li>
 				</ul></li>
             <?php } ?>
+			<li class="<?= $currentActivePage['php'] ?>"><a href="#"><span class="iconfa-refresh"></span> PHP</a><i
+				class="iconfa-angle-right sub-indicator"></i>
+				<ul>
+					<li><a href="/php/php5">PHP5</a></li>
+					<li><a href="/php/symfony">Symfony</a></li>
+				</ul></li>
+			<li class="<?= $currentActivePage['rubypython'] ?>"><a href="#"><span class="iconfa-refresh"></span> Ruby & Python</a><i
+				class="iconfa-angle-right sub-indicator"></i>
+				<ul>
+					<li><a href="/rubypython/python">Python</a></li>
+					<li><a href="/rubypython/ruby">Ruby</a></li>
+					<li><a href="/rubypython/rails">Ruby On Rails</a></li>
+				</ul></li>
+			<li class="<?= $currentActivePage['web'] ?>"><a href="company.html"><span class="iconfa-picture"></span> Web</a><i
+				class="iconfa-angle-right sub-indicator"></i>
+				<ul>
+					<li><a href="/web/firefox-addons">Firefox addons</a></li>
+					<li><a href="/web/tips-web">Trucs web</a></li>
+				</ul></li>
+			<li class="<?= $currentActivePage['xml'] ?>"><a href="/xml"><span class="iconfa-random"></span> XML</a></li>
         </ul>
 	</div>
 	<!--menupanel-->
--- content/en/wiki/frontend/nightwatchjs/nightwatchjs_runTests.php
+++ content/en/wiki/frontend/nightwatchjs/nightwatchjs_runTests.php
@@ -1,17 +1,17 @@
 <section class="wiki-article">
-    <div class="wiki-article-title">
-        <h1 class="btnShowHideArticleBody" data-article-visibility="off">Run tests<h1>
-    </div>
-    <div class="wiki-article-body invisible">
-        <ul>
-        	<li>
-        		When you want to test the project "myProject":
-        		<ul>
-        			<li class="noStyle">
-        				<<?php include "code/nightwatch_code_01.txt"; ?>
-        			</li>
-        		</ul>
-        	</li>
-        </ul>
-    </div>
+	<div class="wiki-article-title">
+		<h1 class="btnShowHideArticleBody" data-article-visibility="off">
+			Run tests
+			<h1>
+	
+	</div>
+	<div class="wiki-article-body invisible">
+		<ul>
+			<li>When you want to test the project "myProject":
+				<ul>
+					<li class="noStyle"><<?php include "code/nightwatch_code_01.txt"; ?></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
 </section>
