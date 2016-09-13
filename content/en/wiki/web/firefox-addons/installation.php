5c699bd1b3ed03b27864e74092164a51850eae2c
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2016-09-13 06:58:57 +0200

    update web wiki

--- content/en/wiki/web/firefox-addons/installation.php
+++ content/en/wiki/web/firefox-addons/installation.php
@@ -1,26 +1,28 @@
 <section class="wiki-article">
-    <div class="wiki-article-title">
-        <h1 class="btnShowHideArticleBody" data-article-visibility="off">Install Add-on SDK<h1>
-    </div>
-    <div class="wiki-article-body invisible">
+	<div class="wiki-article-title">
+		<h1 class="btnShowHideArticleBody" data-article-visibility="off">
+			Install Add-on SDK
+			<h1>
+	
+	</div>
+	<div class="wiki-article-body invisible">
 		<ul>
-			<li>
-				Verify that NodeJS is installed
+			<li>Verify that NodeJS is installed
 				<ul>
 					<li class="noStyle">
 						<?php include 'code/firefox_addons__01.txt'; ?>
 					</li>
-				</ul>	
+				</ul>
 			</li>
-			<li>
-				Installing jpm
+			<li>Installing jpm
 				<ul>
-					<li>
-						The jpm tool is the replacement for <a href="https://developer.mozilla.org/en-US/Add-ons/SDK/Tools/cfx" target="_blank">cfx</a>. It enables you to test, run, and package add-ons.
+					<li>The jpm tool is the replacement for <a
+						href="https://developer.mozilla.org/en-US/Add-ons/SDK/Tools/cfx"
+						target="_blank">cfx</a>. It enables you to test, run, and package add-ons.
 						<?php include 'code/firefox_addons__02.txt'; ?>
 					</li>
-				</ul>	
+				</ul>
 			</li>
 		</ul>
-    </div>
+	</div>
 </section>
--- content/en/wiki/web/tips-web/index.php
+++ content/en/wiki/web/tips-web/index.php
@@ -1,81 +1,84 @@
 <div class="contentpanel" id="wiki">
 	<div class="row-fluid">
 		<div class="span12">
-			<h1 class="wiki-title page-header"><span id="btnShowHideAllArticles" data-article-visibility="off" class="iconfa-plus-sign"></span> Tips and trics Web</h1>
+			<h1 class="wiki-title page-header">
+				<span id="btnShowHideAllArticles" data-article-visibility="off"
+					class="iconfa-plus-sign"></span> Tips and trics Web
+			</h1>
 		</div>
 	</div>
 	<div class="row-fluid">
 		<div class="span12">
 			<section class="wiki-article">
 				<div class="wiki-article-title">
-					<h1 class="btnShowHideArticleBody" data-article-visibility="off">How to create a virtual host on Xampp?<h1>
+					<h1 class="btnShowHideArticleBody" data-article-visibility="off">
+						How to create a virtual host on Xampp?
+						<h1>
+				
 				</div>
 				<div class="wiki-article-body invisible">
-					<p>The goal is to acceed <a href="#">http://localhost:8081/jebalialaeddine.com</a> using <a href="#">http://jebalialaeddine.dev:8081</a></p>
+					<p>
+						The goal is to acceed <a href="#">http://localhost:8081/jebalialaeddine.com</a>
+						using <a href="#">http://jebalialaeddine.dev:8081</a>
+					</p>
 					<h2>Update httpd-vhosts.conf</h2>
 					<p>
-						<ol>
-							<li>
-								Go to "C:\xampp\apache\conf\extra\httpd-vhosts.conf", arround Line-19, add (or uncomment) this line
-								NameVirtualHost *:8081
-							</li>
-							<li>
-								At the bottom of the file add
-								<pre>
-									<code class="xml">
-										#Make "http://localhost:8081/jebalialaeddine.com" accessible via "http://jebalialaeddine.dev:8081"
-										<VirtualHost *:8081>
-										    ServerAdmin alaeddine.jebali@whitecapetech.com
-										    DocumentRoot "C:/xampp/htdocs/jebalialaeddine.com"
-										    ServerName jebalialaeddine.dev
-										    ServerAlias jebalialaeddine.dev
-										    <Directory "C:/xampp/htdocs/jebalialaeddine.com">
-										        Order allow,deny
-										        Allow from all
-										    </Directory>
-										</VirtualHost>
-									</code>
-								</pre>
-							</li>
-							<li>
-						</ol>
+					
+					
+					<ol>
+						<li>Go to "C:\xampp\apache\conf\extra\httpd-vhosts.conf", arround
+							Line-19, add (or uncomment) this line
+							<?php include 'code/02.txt'; ?></li>
+						<li>At the bottom of the file add
+<?php include 'code/01.txt'; ?>
+						</li>
+						<li>
+					
+					</ol>
 					</p>
 					<h2>Update hosts file</h2>
 					<p>
-						<ol>
-							<li>Open Notepad++ as Administrator</li>
-							<li>Open "C:\Windows\System32\drivers\etc\hosts"</li>
-							<li>
-								Go to the bottom of the file and add:
-								<pre>
-									<code class="xml">		
-										127.0.0.1       localhost
-										127.0.0.1       jebalialaeddine.dev
-									</code>
-								</pre>
-							</li>
-							<li>Restart Apache</li>
-						</ol>
+					
+					
+					<ol>
+						<li>Open Notepad++ as Administrator</li>
+						<li>Open "C:\Windows\System32\drivers\etc\hosts"</li>
+						<li>Go to the bottom of the file and add:
+						<?php include 'code/03.txt'; ?>
+						</li>
+						<li>Restart Apache</li>
+					</ol>
 					</p>
 				</div>
 			</section>
 			<section class="wiki-article">
 				<div class="wiki-article-title">
-					<h1 class="btnShowHideArticleBody" data-article-visibility="off">How to change default Xampp port?<h1>
+					<h1 class="btnShowHideArticleBody" data-article-visibility="off">
+						How to change default Xampp port?
+						<h1>
+				
 				</div>
 				<div class="wiki-article-body invisible">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget orci eget erat convallis pellentesque. Nullam tortor mi, molestie quis auctor eu, sodales posuere nulla. Nam vestibulum fringilla urna. Nullam quam urna, sodales sed sem malesuada, iaculis lobortis neque. Phasellus et mi in velit fermentum varius et in sem. Morbi vel quam tempus felis bibendum posuere ut sit amet sem. Phasellus accumsan diam a cursus ultricies. Etiam vehicula lorem enim, vel imperdiet leo accumsan et. Integer iaculis risus lacus. Quisque vel ipsum non lorem varius efficitur vel at ante. Donec ultrices lorem odio. Donec vel tellus ac purus tempus feugiat non ac nulla. Mauris varius dolor eget mattis sollicitudin
-				</div>
+<ul>
+											<li>- Open file "\xampp\apache\conf\httpd.conf"</li>
+											<li>- Search following line "Listen 80".</li>
+											<li>- Change the port 80 to another (like 8081).</li>
+											<li>- Restart Apache.</li>
+											<li>- Go to <a href="http://127.0.0.1:8081" target="_blank">http://127.0.0.1:8081</a>.</li>
+</ul>
+</div>
 			</section>
 			<section class="wiki-article">
 				<div class="wiki-article-title">
-					<h1 class="btnShowHideArticleBody" data-article-visibility="off">How to install Chrome on Ubuntu?<h1>
+					<h1 class="btnShowHideArticleBody" data-article-visibility="off">
+						How to install Chrome on Ubuntu?
+						<h1>
+				
 				</div>
 				<div class="wiki-article-body invisible">
 					<h2>Check if you have 32 or 64 machine</h2>
 					<ul>
-						<li>
-							$> arch
+						<li>$> arch
 							<ul>
 								<li>If you get "i386" => 32 bits</li>
 								<li>If you get "amd64" => 64 bits</li>
@@ -83,20 +86,28 @@
 						</li>
 						<li>Download Chrome</li>
 						<li>$> cd ~/Downloads</li>
-						<li>$> wget https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_i386.deb (for 32 bits)</li>
-						<li>$> wget https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_amd64.deb (for 64bits)</li>
+						<li>$> wget
+							https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_i386.deb
+							(for 32 bits)</li>
+						<li>$> wget
+							https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+							(for 64bits)</li>
 					</ul>
 				</div>
 			</section>
 			<section class="wiki-article">
 				<div class="wiki-article-title">
-					<h1 class="btnShowHideArticleBody" data-article-visibility="off">How to install PHP and Apache on Ubuntu?<h1>
+					<h1 class="btnShowHideArticleBody" data-article-visibility="off">
+						How to install PHP and Apache on Ubuntu?
+						<h1>
+				
 				</div>
 				<div class="wiki-article-body invisible">
 					<h2>Install Apache2</h2>
 					<ul>
 						<li>$> sudo apt-get install apache2</li>
-						<li>To check if Apache is installed go to http://127.0.1.1/ (you should get the message "It works!")</li>
+						<li>To check if Apache is installed go to http://127.0.1.1/ (you
+							should get the message "It works!")</li>
 					</ul>
 					<h2>Restart Apache</h2>
 					<ul>
@@ -112,14 +123,18 @@
 			</section>
 			<section class="wiki-article">
 				<div class="wiki-article-title">
-					<h1 class="btnShowHideArticleBody" data-article-visibility="off">How to install Python on Ubuntu?<h1>
+					<h1 class="btnShowHideArticleBody" data-article-visibility="off">
+						How to install Python on Ubuntu?
+						<h1>
+				
 				</div>
 				<div class="wiki-article-body invisible">
 					<h2>Install some dependencies</h2>
 					<ul>
 						<li>$> sudo apt-get install build-essential checkinstall</li>
-						<li>$> sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev</li>
-						<li>$> </li>
+						<li>$> sudo apt-get install libreadline-gplv2-dev libncursesw5-dev
+							libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev</li>
+						<li>$></li>
 					</ul>
 					<h2>Download Python</h2>
 					<ul>
@@ -141,13 +156,18 @@
 			</section>
 			<section class="wiki-article">
 				<div class="wiki-article-title">
-					<h1 class="btnShowHideArticleBody" data-article-visibility="off">Develop Firefox extensions<h1>
+					<h1 class="btnShowHideArticleBody" data-article-visibility="off">
+						Develop Firefox extensions
+						<h1>
+				
 				</div>
 				<div class="wiki-article-body invisible">
 					<h2>Install</h2>
 					<ul>
 						<li>Install Python as described above</li>
-						<li>Download the SDK from <a herf="https://ftp.mozilla.org/pub/mozilla.org/labs/jetpack/jetpack-sdk-latest.tar.gz" target="_blank">https://ftp.mozilla.org/pub/mozilla.org/labs/jetpack/jetpack-sdk-latest.tar.gz</a></li>
+						<li>Download the SDK from <a
+							herf="https://ftp.mozilla.org/pub/mozilla.org/labs/jetpack/jetpack-sdk-latest.tar.gz"
+							target="_blank">https://ftp.mozilla.org/pub/mozilla.org/labs/jetpack/jetpack-sdk-latest.tar.gz</a></li>
 						<li>$> tar -xf addon-sdk.tar.gz</li>
 						<li>$> cd addon-sdk</li>
 					</ul>
@@ -155,35 +175,39 @@
 			</section>
 			<section class="wiki-article">
 				<div class="wiki-article-title">
-					<h1 class="btnShowHideArticleBody" data-article-visibility="off">Base URL in HTML<h1>
+					<h1 class="btnShowHideArticleBody" data-article-visibility="off">
+						Base URL in HTML
+						<h1>
+				
 				</div>
 				<div class="wiki-article-body invisible">
 					<h2>Sub title</h2>
 					<ul>
-						<li style="noStyle">
-							<?php include "code/tips_web_code_01.txt"; ?>
+						<li style="">
+							<?php include "code/04.txt"; ?>
 						</li>
 					</ul>
 				</div>
 			</section>
 
-	
-	
 
-	
+
+
+
 
 
 
 			<section class="wiki-article">
 				<div class="wiki-article-title">
-					<h1 class="btnShowHideArticleBody" data-article-visibility="off">Title<h1>
+					<h1 class="btnShowHideArticleBody" data-article-visibility="off">
+						Title
+						<h1>
+				
 				</div>
 				<div class="wiki-article-body invisible">
 					<h2>Sub title</h2>
 					<ul>
-						<li>
-							
-						</li>
+						<li></li>
 					</ul>
 				</div>
 			</section>
