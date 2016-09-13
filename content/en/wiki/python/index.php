b6730b00fd51f6a595f2bd2ba438f879005f464a
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2016-09-13 07:08:29 +0200

    update wiki

--- content/en/wiki/python/index.php
+++
@@ -1,6 +0,0 @@
-Installation
-If you get this error when opening your Python project (or a python file) with Eclipse: "eclipse it seems that python interpreter is not currently configured":
-
-See your Python path: $> which python
-Open Eclipse -> preferences -> PyDev -> Interpreter -> Pyhton interpreter  -> New -> Browse to location displayed when running "which python"
-
