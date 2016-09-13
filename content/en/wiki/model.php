d42024a92798045a9c8b4d94fe9213763173300a
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2016-09-13 07:06:18 +0200

    update model

--- content/en/wiki/model.php
+++ content/en/wiki/model.php
@@ -1,9 +1,12 @@
 <section class="wiki-article">
 	<div class="wiki-article-title">
-    	<h1 class="btnShowHideArticleBody" data-article-visibility="off">Title<h1>
-    </div>
-    <div class="wiki-article-body invisible">
-    	<ul>
+		<h1 class="btnShowHideArticleBody" data-article-visibility="off">
+			Title
+			<h1>
+	
+	</div>
+	<div class="wiki-article-body invisible">
+		<ul>
 			<li></li>
 		</ul>
 	</div>
