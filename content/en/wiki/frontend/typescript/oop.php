5f0478f02a96736da1beda0cafadc2c9202ae6fa
Ala Eddine JEBALI <ajebali.ext@orange.com>
2016-09-30 17:07:14 +0200

    Update TS

--- content/en/wiki/frontend/typescript/oop.php
+++ content/en/wiki/frontend/typescript/oop.php
@@ -18,6 +18,36 @@
 						By default a class attributes are "public". To simulate getters and setters:
 						<?php include 'code/24.txt'; ?>
 					</li>
+					<li>
+						Classes in Typescript act just as a container that encapsulate a code. For example, the generated JS code of above class will be
+						<?php include 'code/26.txt'; ?>
+					</li>
+					<li>
+						Class properties (getters and setters) act as filters.
+					</li>
+					<li>
+						Remark that in the TS class above, we name the attribute "_name" (recommended) not "name" because TS will generate a property "name" for getters and setters. Otherwise it'll not know which "name" you're talking about (the attribute or the getter/setter)!
+					</li>
+					<li>
+						When you define a method name "get" or "set" followed by &lt;aVariable&gt;, TS will consider it as a getter/setter of the attribute used inside those 2 methods.
+						<ul>
+							<li>
+								This only works with TS5 or higher.								
+							</li>
+							<li>
+								If you target ES3, you have 2 options:
+								<ul>
+									<li>
+										Make the class attributes public (this not good since it doesn't respect OOP encapsulation).
+									</li>
+									<li>
+										Simulate getters and setters
+										<?php include 'code/27.txt'; ?>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</li>
 				</ul>
 			</li>
 			<li>
@@ -42,6 +72,18 @@
 				<ul>
 					<li>
 						<?php include 'code/13.txt'; ?>
+					</li>
+				</ul>
+			</li>			
+			<li>
+				<h2>Interfaces</h2>
+				<ul>
+					<li>
+						Interfaces define a contract that other objects must respect (implement).
+					</li>
+					<li>
+						Example: In a factory that produces engines, each engine need to be able to "start" and "stop".
+						<?php include 'code/28.txt'; ?>
 					</li>
 				</ul>
 			</li>
---
+++ content/en/wiki/frontend/typescript/v2.php
@@ -0,0 +1,20 @@
+<section class="wiki-article">
+	<div class="wiki-article-title">
+		<h1 class="btnShowHideArticleBody" data-article-visibility="off">
+			Generics
+			<h1>
+	
+	</div>
+	<div class="wiki-article-body invisible">
+		<ul>
+			<li>
+				<h2>Presentation</h2>
+				<ul>
+					<li>
+						See this https://blogs.msdn.microsoft.com/typescript/2016/09/22/announcing-typescript-2-0/
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</section>
