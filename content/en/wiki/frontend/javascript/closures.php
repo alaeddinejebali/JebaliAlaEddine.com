3dbf6843023324350e61e02b6d5d28216a4fb90e
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2016-11-13 20:13:06 +0100

    update javascript wiki

---
+++ content/en/wiki/frontend/javascript/closures.php
@@ -0,0 +1,41 @@
+
+<section class="wiki-article">
+	<div class="wiki-article-title">
+		<h1 class="btnShowHideArticleBody" data-article-visibility="off" id="closures">
+			Closures
+			<h1>
+	
+	</div>
+	<div class="wiki-article-body invisible">
+		<ul>
+			<li>
+				<h2>Presentations</h2>
+				<ul>
+					<li>
+						Closures (or enclosures) are similar to nested functions in JAVA.
+					</li>
+					<li>
+						It's a function inside another function.
+					</li>
+					<li>
+						A closure has access to:
+						<ul>
+							<li>
+								its own variables.
+							</li>
+							<li>
+								outer functions variables and parameters.
+							</li>
+							<li>
+								global variables.
+							</li>
+						</ul>
+					</li>
+					<li>
+						
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</section>
