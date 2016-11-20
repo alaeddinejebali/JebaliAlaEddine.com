e151851fb20a474d235d8438cc46c4b786efc95f
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2016-11-20 18:49:54 +0100

    update front

---
+++ content/en/wiki/frontend/javascript/index.php
@@ -0,0 +1,27 @@
+<div class="contentpanel" id="wiki">
+	<div class="row-fluid">
+		<div class="span12">
+			<h1 class="wiki-title page-header">
+				<span id="btnShowHideAllArticles" data-article-visibility="off"
+					class="iconfa-plus-sign"></span> JavaScript
+			</h1>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+			<?php include "presentation.php"; ?>
+			<?php include "scope.php"; ?>
+			<?php include "closures.php"; ?>
+			<?php include "advancedtopics.php"; ?>
+			<?php //include "javascript__dataTypes.php"; ?>
+			<?php //include "javascript__loopsAndFunctions.php"; ?>
+			<?php //include "javascript__dataTypes.php"; ?>
+			<?php //include "javascript__oop.php"; ?>
+			<?php //include "javascript__browser.php"; ?>
+			<?php //include "javascript__oop.php"; ?>
+			<?php //include "javascript__browser.php"; ?>
+			<?php //include "javascript__cookies.php"; ?>
+			<?php include "tips.php"; ?>
+		</div>
+	</div>
+</div>
