8f35c6136c9954b04b5c68f022192b919cdfd984
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2016-11-20 18:50:42 +0100

    update wiki

---
+++ content/en/wiki/rubypython/python/index.php
@@ -0,0 +1,15 @@
+<div class="contentpanel" id="wiki">
+	<div class="row-fluid">
+		<div class="span12">
+			<h1 class="wiki-title page-header">
+				<span id="btnShowHideAllArticles" data-article-visibility="off"
+					class="iconfa-plus-sign"></span> Python
+			</h1>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+            <?php include "introduction.php"; ?>
+        </div>
+	</div>
+</div>
