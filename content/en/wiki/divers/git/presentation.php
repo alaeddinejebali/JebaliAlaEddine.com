625fc26350cc381eeddbf98152a4a3a13b06ecaa
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2016-12-08 10:53:28 +0100

    Create presentation.php

---
+++ content/en/wiki/divers/git/presentation.php
@@ -0,0 +1,23 @@
+<section class="wiki-article">
+	<div class="wiki-article-title">
+		<h1 class="btnShowHideArticleBody" data-article-visibility="off">
+			Presentation
+			<h1>
+	
+	</div>
+	<div class="wiki-article-body invisible">
+		<ul>
+			<li>
+        <h2>Presentation</h2>
+        <ul>
+          <li>
+            Upgrade from v1 to v2
+            <li class="noStyle"><?php include "code/git_code_33.txt"; ?></li>
+            It's not always necessary to remove the existing Git before upgrading it, but if you run into any problems, do the following and then repeat the steps mentioned above:
+            <li class="noStyle"><?php include "code/git_code_34.txt"; ?></li>
+          </li>
+        </ul>
+      </li>
+		</ul>
+	</div>
+</section>
