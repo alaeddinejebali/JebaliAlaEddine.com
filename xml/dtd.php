930c454cc5d7ec372eb25dee89f0e4d67de590be
Ala Eddine JEBALI <ajebali.ext@orange.com>
2017-02-10 17:24:01 +0100

    update xml

---
+++ xml/dtd.php
@@ -0,0 +1,29 @@
+<div class="panel panel-default wiki-article">
+	<div class="panel-heading wiki-article-title">
+		<h4 class="panel-title">
+        	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#dtd">DTD (Document Type Definition)</a>
+		</h4>
+	</div>
+	<div id="dtd" class="panel-collapse collapse">
+		<div class="panel-body wiki-article-body">
+			<ul>
+				<li>
+					A "valid" XML document must be well formed. In addition, it must conform to a document type definition.
+				</li>
+				<li>
+					A document type definition defines the rules and the legal elements and attributes for an XML document.
+				</li>
+				<li>
+					There are two different document type definitions that can be used with XML:
+					<ul>
+						<li>DTD: The original Document Type Definition</li>
+						<li>XML Schema - An XML-based alternative to DTD</li>
+					</ul>
+				</li>
+				<li>
+					To be continued (see <a href="http://www.w3schools.com/xml/xml_dtd.asp">http://www.w3schools.com/xml/xml_dtd.asp</a>)
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
---
+++ xml/index.php
@@ -0,0 +1,8 @@
+<div class="about_tabbed">     
+	<div class="panel-group" id="accordion">
+		<?php
+			include "basics.php";
+			include "dtd.php";
+		?>					
+	</div>
+</div>
