899f45ae81aa2075ce16260e5aedbeee46876539
Ala Eddine JEBALI <ajebali.ext@orange.com>
2017-02-10 17:23:27 +0100

    update docker

---
+++ docker/presentation.php
@@ -0,0 +1,51 @@
+<div class="panel panel-default wiki-article">
+	<div class="panel-heading wiki-article-title">
+		<h4 class="panel-title">
+        	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#presentation">Presentation</a>
+		</h4>
+	</div>
+	<div id="presentation" class="panel-collapse collapse">
+		<div class="panel-body wiki-article-body">
+			<ul>
+				<li>
+					<h2>Installation</h2>
+					<ul>
+						<li>
+							Install Docker:
+							<ul>
+								<li class="noStyle">
+                                	<?php include "code/git_code_01.txt"; ?>
+                                </li>
+                            </ul>
+						</li>
+						<li>
+							Install Docker-compose:
+							<ul>
+								<li class="noStyle">
+                                	<?php include "code/git_code_03.txt"; ?>
+                                </li>
+								<li>
+									Install docker machine
+									<ul>
+										<li class="noStyle">
+											<?php include 'code/git_code_04.txt'; ?>
+										</li>
+									</ul>
+								</li>
+                            </ul>
+						</li>
+						<li>
+							If you encounter this problem: "The following signatures couldn't be verified because the public key is not available: NO_PUBKEY 16126D3A3E5C1192"
+							<ul>
+								<li>
+                                    You can resolve it like that:
+                                    <?php include "code/git_code_02.txt"; ?>
+                                </li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
