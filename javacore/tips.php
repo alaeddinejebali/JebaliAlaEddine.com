24d87f8a67000da9320bd1d88193bbf764a79684
Ala Eddine JEBALI <ajebali.ext@orange.com>
2017-03-16 16:21:03 +0100

    Add javacore tips

---
+++ javacore/tips.php
@@ -0,0 +1,21 @@
+<div class="panel panel-default wiki-article">
+	<div class="panel-heading wiki-article-title">
+		<h4 class="panel-title">
+        	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#tipsandtricks">Tips and Trocks</a>
+		</h4>
+	</div>
+	<div id="tipsandtricks" class="panel-collapse collapse">
+		<div class="panel-body wiki-article-body">
+			<ul>
+				<li>
+					<h2>Online Java execution</h2>
+					<ul>
+						<li>
+							<a href="https://www.compilejava.net" target="_blank">https://www.compilejava.net</a>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
