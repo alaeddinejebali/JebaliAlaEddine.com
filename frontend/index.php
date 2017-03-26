0e9a89fcec2c98b9937593dca7e352d72801c795
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2017-03-26 22:43:02 +0200

    update front

--- frontend/index.php
+++ frontend/index.php
@@ -1,5 +1,5 @@
 <div class="about_tabbed">     
 	<div class="panel-group" id="accordion">
-		<?php //include "presentation.php"; ?>					
+		<?php include "introduction.php"; ?>
 	</div>
 </div>
---
+++ frontend/introduction.php
@@ -0,0 +1,185 @@
+<div class="panel panel-default wiki-article">
+	<div class="panel-heading wiki-article-title">
+		<h4 class="panel-title">
+        	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#presentation">Introduction</a>
+		</h4>
+	</div>
+	<div id="presentation" class="panel-collapse collapse">
+		<div class="panel-body wiki-article-body">
+		<ul>
+			<li>
+				<h2>aaa</h2>
+				<ul>
+					<li>
+						What is a fron-end development?
+						<ul>
+							<li>
+								Anything related to a browser (HTML, JS, CSS, images)
+							</li>
+							<li>
+								It's not related to how a server work.
+							</li>
+						</ul>
+					</li>
+					<li>
+						Technologies
+						<ul>
+							<li>
+								<a href="" target="_blank"></a>
+							</li>
+							<li>
+								<a href="" target="_blank"></a>
+							</li>
+							<li>
+								<a href="" target="_blank"></a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						MDN (Microsoft Developer Network)
+					</li>
+					<li>
+						Online test HTML/JS code
+						<ul>
+							<li>
+								<a href="http://jsfiddle.net" target="_blank">http://jsfiddle.net</a>
+								<ul>
+									<li>
+										Advantages:
+										<ul>
+											<li>
+												Format JS code
+											</li>
+											<li>
+												Save different versions each time you save.
+											</li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li>
+								<a href="http://jsbin.com" target="_blank">http://jsbin.com</a>
+								<ul>
+									<li>
+										Advantages:
+										<ul>
+											<li>
+												Live output of a result.
+											</li>
+											<li>
+												Separate console area.
+											</li>
+											<li>
+												Export the code to by directly creating a github project.
+											</li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li>
+								<a href="http://codepen.io" target="_blank">http://codepen.io</a>
+							</li>
+							<li>
+								<a href="http://plnker.co" target="_blank">http://plnkr.co</a>
+								<ul>
+									<li>
+										Advantages:
+										<ul>
+											<li>
+												Create and edit separate files.
+											</li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+					<li>
+						HTML5 elements
+						<ul>
+							<li>
+								Audio
+							</li>
+							<li>
+								Video
+							</li>
+							<li>
+								Local storage
+							</li>
+							<li>
+								Session storage
+							</li>
+							<li>
+								Canvas
+							</li>
+							<li>
+								Offline
+								<ul>
+									&lt;html manifest="app.appcache" &gt;
+								</ul>
+							</li>
+							<li>
+								History
+							</li>
+							<li>
+								Geolocation
+							</li>
+						</ul>
+					</li>
+					
+					
+					<li>
+						CDN
+						<ul>
+							<li>
+								Content Delivery Network
+							</li>
+							<li>
+								Adavantages:
+								<ul>
+									<li>
+										Content can be pre-cached.
+									</li>
+									<li>
+										Distributed regional server.
+									</li>
+									<li>
+										High capacity infrastucture
+									</li>
+									<li>
+										Reduced load and bandwidth.
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+					<li>
+						Minification
+						<ul>
+							<li class="caution">
+								Transeferring 4 files sized each 2ko to a server <strong>takes longer time</strong> than transferring 1 file sized 4ko.
+							</li>
+							<li>
+								For a browser, having 4 files or 1 file does not matter.
+							</li>
+							<li>
+								Objective: reduce size of files.
+								<ul>
+									<li>
+										Rename variables to short chars (a, b, c...etc)
+									</li>
+									<li>
+										Remove unecessary space
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+					<li>
+						
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</div>
