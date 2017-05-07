6c4cf0db8cc85eed0861113c1b8b53a003c970b1
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2017-05-07 11:31:37 +0000

    Add package managers

---
+++ frontpackagemanagers/index.php
@@ -0,0 +1,8 @@
+<div class="about_tabbed">     
+    <div class="panel-group" id="accordion">
+        <?php
+            include "presentation.php";
+            include "npm.php";
+        ?>
+    </div>
+</div>
