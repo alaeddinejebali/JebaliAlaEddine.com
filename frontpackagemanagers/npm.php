1a28b7f890bfb55263c523bafa97135581359dd1
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2017-05-07 11:32:39 +0000

    add package managers

---
+++ frontpackagemanagers/npm.php
@@ -0,0 +1,24 @@
+<div class="panel panel-default wiki-article">
+	<div class="panel-heading wiki-article-title">
+		<h4 class="panel-title">
+        	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#npm">NPM</a>
+		</h4>
+	</div>
+	<div id="npm" class="panel-collapse collapse">
+		<div class="panel-body wiki-article-body">
+			<ul>
+				<li>
+					<h2>Presentation</h2>
+					<ul>
+						<li>
+                            NPM (Node Package Manage) is a module manager for JavaScript (front and back).
+						</li>
+						<li>
+                            It's replacing Bower
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
