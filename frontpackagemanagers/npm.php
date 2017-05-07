a0b0a1f4387c79c90043909bd0f757cbc5b1dca0
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2017-05-07 15:37:29 +0000

    Add npm

--- frontpackagemanagers/npm.php
+++ frontpackagemanagers/npm.php
@@ -16,6 +16,20 @@
 						<li>
                             It's replacing Bower
 						</li>
+						<li>
+						      Install NPM
+						      <ul>
+						      
+						      </ul>
+						</li>
+						<li>
+						      Upgrade to latest version
+						      <ul>
+						          <li>
+						              $> npm install -g npm@latest
+						          </li>
+						      </ul>
+						</li>
 					</ul>
 				</li>
 				<li>
@@ -109,6 +123,22 @@
 				<li>
 				    <h2>Packages installation</h2>
 				    <ul>
+				        <li>
+				            $> npm isnatll &lt;urlToGitRepositoryOfThePackage&gt;
+				            <ul>
+				                <li>
+				                    Install the package from it's git repository
+				                </li>
+				            </ul>
+				        </li>
+				        <li>
+				            $> npm isnatll &lt;path/to/folder&gt;
+				            <ul>
+				                <li>
+				                    Install the package from a folder.
+				                </li>
+				            </ul>
+				        </li>
 				        <li>
 				            $> npm install -g &lt;packageName&gt;
 				            <ul>
@@ -217,6 +247,225 @@
 				        </li>
 				    </ul>
 				</li>
+				<li>
+				    <h2>Install specific version</li>
+				    <ul>
+				        <li>
+				            vx.y.z
+				            <ul>
+				                <li>
+				                    x: major version
+				                </li>
+				                <li>
+				                    y: minor version (new features added...)
+				                </li>
+				                <li>
+				                    x: patch / revision version (bug fixes...)
+				                </li>
+				            </ul>
+				        </li>
+				        <li>
+				            $> npm install &lt;packageName&gt;@1.8.2
+				        </li>
+				        <li>
+				            $> npm install &lt;packageName&gt;@1.8.x
+				            <ul>
+				                <li>
+				                    Install latest version of v1.8
+				                </li>
+				                <li>
+				                    Is equivalent to: $> npm install &lt;packageName&gt;@1.8
+				                </li>
+				            </ul>
+				        </li>
+				        <li>
+				            $> npm install &lt;packageName&gt;@">=1.1.0 <1.4.0"
+				        </li>
+				        <li>
+				            Install from <i>package.json</i> (when running "$> npm install")
+				            <ul>
+				                <li>
+				                    ^x.y.z: install latest version of major release.
+				                </li>
+				                <li>
+				                    ~x.y.z: install latest version of minor release.
+				                </li>
+				            </ul>
+				        </li>
+				    </ul>
+				</li>
+                <li>
+                	<h2>Update packages</h2>
+                	<ul>
+                		<li>
+                			$> npm update --save
+                		</li>
+                		<li>
+                			$> npm update --prod
+                		</li>
+                		<li>
+                			$> npm update -g &lt;packageName&gt;
+                			<ul>
+                				<li>
+                					upadate the installed globally version
+                				</li>
+                			</ul>
+                		</li>
+                		<li>
+                			$> npm update &lt;packageName&gt;
+                			<ul>
+                				<li>
+                					Get the latest version
+                				</li>
+                			</ul>
+                		</li>
+                	</ul>
+                </li>
+                <li>
+                	<h2>Prunning</h2>
+                	<ul>
+                		<li>
+                			It's useful when you're preparing going to prod.
+                			<ul>
+                				<li>
+                					Imagine that you or another memeber of the team used "$> npm remove &lt;packageName&gt;" by mistake.
+                				</li>
+                				<li>
+                					As there'll be no alert, the project may not work in production
+                				</li>
+                			</ul>
+                		</li>
+                		<li>
+                			$> npm list --depth 0
+                			<ul>
+                				<li>
+                					In case you see "extraneous" near a package name, it means that this package is not specified in <i>package.json</i> file.
+                				</li>
+                			</ul>
+                		</li>
+                		<li>
+                			$> npm prune
+                			<ul>
+                				<li>
+                					Compare installed packages with ones found in <i>package.json</i> file and then fixes problems.
+                				</li>
+                			</ul>
+                		</li>
+                		<li>
+                			$> npm prune &lt;packageName&gt;
+                		</li>
+                		<li>
+                			$> npm prune --production
+                			<ul>
+                				<li>
+                					Will remove all "dev" packages.
+                				</li>
+                			</ul>
+                		</li>
+                	</ul>
+                </li>
+                <li>
+                	<h2>NPM registery</h2>
+                	<ul>
+                		<li>
+                			You can look for a package from <a href="http://npmjs.com" target="_blank"></a>.
+                		</li>
+                		<li>
+                			Searching for a package
+                			<ul>
+                				<li>
+                					$> npm search &lt;aString&gt;
+                					<ul>
+                						<li>
+                							Looks for packages having that word in their names and/or in their descriptions.
+                						</li>
+                						<li>
+                							The problem is that you may get too many results, so <a href="http://npmjs.com" target="_blank"></a> is better.
+                						</li>
+                					</ul>
+                				</li>
+                			</ul>
+                		</li>
+                		<li>
+                			$> npm repo &lt;packageName&gt;
+                			<ul>
+                				<li>
+                					Open package repository page
+                				</li>
+                			</ul>
+                		</li>
+                		<li>
+                			Publish your own package
+                			<ul>
+                				<li>
+                					Start by creating an account on <a href="http://npmjs.com" target="_blank"></a>.
+                				</li>
+                				<li>
+                					Prepare your project for publishing
+                					<ul>
+                						<li>
+                							Setup the project for GIT.
+                						</li>
+                						<li>
+                							create a <i>package.json</i> file.
+                						</li>
+                					</ul>
+                				</li>
+                				<li>
+                					Publish to NPM
+                					<ul>
+                						<li>
+                							$> npm publish
+                						</li>
+                						<li>
+                							$> npm info &lt;packageName&gt;
+                						</li>
+                						<li>
+                							$> npm repo &lt;packageName&gt;
+                						</li>
+                						<li>
+                							See your project at <a href="http://npmjs.im/&lt;packageName&gt;" target="_blank"></a>
+                						</li>
+                					</ul>
+                				</li>
+                				<li>
+                					Publish an update
+                					<ul>
+                						<li>
+                							Make you changes.
+                						</li>
+                						<li>
+                							Update the version of the project. There are 2 ways:
+                							<ol>
+                								<li>
+                									Update <i>package.json</i>.
+                								</li>
+                								<li>
+                									Automatically update of <i>package.json</i> by tapping:
+                									<ul>
+                										<li>
+                											$> npm version major
+                										</li>
+                										<li>
+                											$> npm version minor
+                										</li>
+                										<li>
+                											$> npm version patch
+                										</li>
+                									</ul>
+                									Using this way, NPM will create a new commit and a new tag will be created! So you need to run "$> npm publish --tags"
+                								</li>
+                							</o>
+                						</li>
+                						<li>
+                							$> npm publish
+                						</li>
+                					</ul>
+                				</li>	
+                			</ul>
+                		</li>
+                	</ul>
+                </li>
 			</ul>
 		</div>
 	</div>
