40170bef6402aa14b67edc71561f9fe51a74ea3b
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2017-05-11 16:22:35 +0200

    Update index.php

--- oop/index.php
+++ oop/index.php
@@ -6,26 +6,67 @@
 	</div>
 	<div id="oop" class="panel-collapse collapse">
 		<div class="panel-body wiki-article-body">
-			<ul>
-				<li>
-					<h2>aaa</h2>
-					<ul>
-						<li>
-							An object is a runtime instance of a class in memory.
-						</li>
-			            <li>Classes have two primary elements (known as members of
-							the class):
-							<ul>
-								<li>Methods (often called functions or procedures in other
-									languages).</li>
-								<li>Fields (variables).</li>
-							</ul>
-						</li>
-						<li>Method signature: The full declaration of a method: parameters
-							type and return type of the method.</li>						
-					</ul>
-				</li>
-			</ul>
+<ul>
+	<li>
+		<h2>Polymorphism</h2>
+		<ul>
+			<li>
+				This is an object oriented concept where same function can be used for different purposes. For example function name will remain same but it make take different number of arguments and can do different task.
+			</li>
+		</ul>
+	</li>
+	<li>
+		<h2>Encapsulation</h2>
+		<ul>
+			<li>
+				encapsulation is the hiding of data implementation by restricting access to accessors and mutators. 
+			</li>
+		</ul>
+	</li>
+	<li>
+		<h2>Abstraction</h2>
+		<ul>
+			<li>
+				Abstraction is used to manage complexity. Software developers use abstraction to decompose complex systems into smaller components.
+			</li>
+		</ul>
+	</li>
+	<li>
+		<h2>Object</h2>
+		<ul>
+			<li>
+				An individual instance of the data structure defined by a class. You define a class once and then make many objects that belong to it. Objects are also known as instance.
+			</li>
+		</ul>
+	</li>
+	<li>
+		<h2>Private members</h2>
+		<ul>
+			<li>
+				The member is only  accessible by the class in which it is declared.
+			</li>
+		</ul>
+	</li>
+	<li>
+		<h2>Protected members</h2>
+		<ul>
+			<li>
+				A protected property or method is only accessible in the class in which it is declared, as well as in classes that extend that class.
+			</li>
+		</ul>
+	</li>
+	<li>
+		<h2>Abstract class</h2>
+		<ul>
+			<li>
+				An abstract class is one that cannot be instantiated, only inherited. 
+			</li>
+			<li>
+				Function definitions inside an abstract class must also be abstract. It is not legal to have abstract function definitions inside a non-abstract class.
+			</li>
+		</ul>
+	</li>
+</ul>
 		</div>
 	</div>								
 </div>
